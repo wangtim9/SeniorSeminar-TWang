6a971affb2374f2dfc026873a923642b5571adc5
wangtim9 <wangtim27@7hills.org>
2026-02-09 09:39:08 -0500

    Add files via upload
    started on a counter to count how many choices each seminar got to rank popularity

---
+++ Counter.java
@@ -0,0 +1,48 @@
+import java.io.*;
+
+public class Counter {
+	int one = 0;
+	int two = 0;
+	int three = 0;
+	int four = 0;
+	int five = 0;
+	int six = 0;
+	int seven = 0;
+	int eight = 0;
+	int nine = 0;
+	int ten = 0;
+	int eleven = 0;
+	int twelve = 0;
+	int thirteen = 0;
+	int fourteen = 0;
+	int fifteen = 0;
+	int sixteen = 0;
+	int seventeen = 0;
+	int eighteen = 0
+	public status void main(String[] args) throws IOException {
+		File Choice = new File(choices.txt);
+		Scanner scan = new Scanner(Choice);
+		if ((scan.nextInt()).equals(1)) {
+			one++;
+		}
+		else if ((scan.nextInt()).equals(2)) {
+			two++;
+		}
+		else if ((scan.nextInt()).equals(3)) {
+			three++;
+		}
+		else if ((scan.nextInt()).equals(4)) {
+			four++;
+		}
+		else if ((scan.nextInt()).equals(5)) {
+			five++;
+		}
+		else if ((scan.nextInt()).equals(6)) {
+			six++;
+		}
+		else if ((scan.nextInt()).equals(7)) {
+			three++;
+		}
+	}
+	
+	
