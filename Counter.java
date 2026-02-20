b2181ae71653be951af4d8f28628e5b08b4701d6
wangtim9 <wangtim27@7hills.org>
2026-02-20 16:16:51 -0500

    Update Counter.java "what am I doing?"

--- Counter.java
+++ Counter.java
@@ -1,48 +1,28 @@
 import java.io.*;
 
 public class Counter {
-	int one = 0;
-	int two = 0;
-	int three = 0;
-	int four = 0;
-	int five = 0;
-	int six = 0;
-	int seven = 0;
-	int eight = 0;
-	int nine = 0;
-	int ten = 0;
-	int eleven = 0;
-	int twelve = 0;
-	int thirteen = 0;
-	int fourteen = 0;
-	int fifteen = 0;
-	int sixteen = 0;
-	int seventeen = 0;
-	int eighteen = 0
-	public status void main(String[] args) throws IOException {
-		File Choice = new File(choices.txt);
-		Scanner scan = new Scanner(Choice);
-		if ((scan.nextInt()).equals(1)) {
-			one++;
-		}
-		else if ((scan.nextInt()).equals(2)) {
-			two++;
-		}
-		else if ((scan.nextInt()).equals(3)) {
-			three++;
-		}
-		else if ((scan.nextInt()).equals(4)) {
-			four++;
-		}
-		else if ((scan.nextInt()).equals(5)) {
-			five++;
-		}
-		else if ((scan.nextInt()).equals(6)) {
-			six++;
-		}
-		else if ((scan.nextInt()).equals(7)) {
-			three++;
-		}
-	}
+	int private one = 0;
+	int private two = 0;
+	int private three = 0;
+	int private four = 0;
+	int private five = 0;
+	int private six = 0;
+	int private seven = 0;
+	int private eight = 0;
+	int private nine = 0;
+	int private ten = 0;
+	int private eleven = 0;
+	int private twelve = 0;
+	int private thirteen = 0;
+	int private fourteen = 0;
+	int private fifteen = 0;
+	int private sixteen = 0;
+	int private seventeen = 0;
+	int private eighteen = 0
+	public void howMany() {
+		for (int i = 0; i > 17; i++) {
+			for (int n = 0; i > 73; i++) {
+				if (((SeminarList.get(i)).getID()).equals((StudentList.get(n)).getC1())) {
+					
 	
 	
