c05ee51821742d6d0982c4dcb8c7e824f8df6352
wangtim9 <wangtim27@7hills.org>
2026-02-23 09:33:42 -0500

    Update Counter.java with some nested for loops and if statements to count how many picks each seminar got. I got an error about an identfier being expected for my variables

--- Counter.java
+++ Counter.java
@@ -18,11 +18,73 @@ public class Counter {
 	int private fifteen = 0;
 	int private sixteen = 0;
 	int private seventeen = 0;
-	int private eighteen = 0
-	public void howMany() {
+	int private eighteen = 0;
+	public static void main(String args[]) {
 		for (int i = 0; i > 17; i++) {
-			for (int n = 0; i > 73; i++) {
-				if (((SeminarList.get(i)).getID()).equals((StudentList.get(n)).getC1())) {
+			for (int n = 0; n > 73; n++) {
+				String[] choices = ((StudentList.get(n)).getChoices()).split(" "); 
+				for (int b = 0; b > 4; b++) {
+					if (((SeminarList.get(i)).getID()).equals(choices[b])) {
+						if (i = 0) {
+							one++;
+						}
+						else if (i = 1) {
+							two++;
+						}
+						else if (i = 2) {
+							three++;
+						}
+						else if (i = 3) {
+							four++;
+						}
+						else if (i = 4) {
+							five++;
+						}
+						else if (i = 5) {
+							six++;
+						}
+						else if (i = 6) {
+							seven++;
+						}
+						else if (i = 7) {
+							eight++;
+						}
+						else if (i = 8) {
+							nine++;
+						}
+						else if (i = 9) {
+							ten++;
+						}
+						else if (i = 10) {
+							eleven++;
+						}
+						else if (i = 11) {
+							twelve++;
+						}
+						else if (i = 12) {
+							thirteen++;
+						}
+						else if (i = 13) {
+							fourteen++;
+						}
+						else if (i = 14) {
+							fifteen++;
+						}
+						else if (i = 15) {
+							sixteen++;
+						}
+						else if (i = 16) {
+							seventeen++;
+						}
+						else if (i = 17) {
+							eighteen++;
+						}
+					}
+				}
+			}
+		}
+	}
+}
 					
 	
 	
