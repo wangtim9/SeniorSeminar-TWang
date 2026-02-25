70cfcd774b657c776661b6d4fea35994c44621b8
wangtim9 <wangtim27@7hills.org>
2026-02-24 21:09:50 -0500

    Update Sessions.java with comments

--- Sessions.java
+++ Sessions.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
-
+//constructor
 public class Sessions {
 	private String Seminar_name;
 	private String SeminarID;
@@ -10,20 +10,26 @@ public class Sessions {
 		Seminar_name = name;
 		SeminarID = ID;
 	}
+	//getters
 	public String getName() {
 		return Seminar_name;
 	}
 	public String getID() {
 		return SeminarID;
 	} 
+	//Here I use scannerr ot read in a file with the list of session names and ids
 	public static void main (String[] args) {
+		//initializaation of arraylist
 		ArrayList<Sessions> SeminarList = new ArrayList<Sessions>();
 		File Seminars = new File("seminars.txt");
 		try (Scanner Scan = new Scanner(Seminars)) {
 			while (Scan.hasNextLine()) {
 				String data = Scan.nextLine();
+				//split by commas
 				String[] IDs = data.split(",");
+				//object is created
 				Sessions Seminar = new Sessions(IDs[0], IDs[1]);
+				//object is added to arraylist
 				SeminarList.add(Seminar);
 			}
 			//System.out.println((SeminarList.get(0)).getName());
@@ -34,3 +40,4 @@ public class Sessions {
 	}
 }
 			
+
