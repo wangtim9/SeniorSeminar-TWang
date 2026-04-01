3d5af3bdfbd1052316147da52846792b1658314e
wangtim9 <wangtim27@7hills.org>
2026-04-01 09:36:01 -0400

    Add files via upload

---
+++ SeniorSeminar.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class SeniorSeminar {
+	public SeniorSeminar() {
+		ArrayList<Student> StudentList = new ArrayList<Student>();
+		ArrayList<Sessions> SeminarList = new ArrayList<Sessions>();
+		ArrayList[][] schedule = new ArrayList[5][5];
+	}
+}
